329a7eabe0731764ffec0fc8a5fffb582cc7c0bc
suxiaofei1 <544785458@qq.com>
2021-08-11 16:45:34 +0800

    first commit

---
+++ scripts/test_aaaa.py
@@ -0,0 +1,19 @@
+import time
+import allure
+import pytest
+
+
+
+class Test_DengLu():
+
+
+    def test_name_password(self):
+
+        assert 1
+
+    def test_name_password2(self):
+
+        assert 0
+
+
+
